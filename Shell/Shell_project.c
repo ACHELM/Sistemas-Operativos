2040083bd24c84eeb9e37c3e6e743ed5f4e7935f
Amin Chaloukh El Mohammadi <aminchaloukh@gmail.com>
2024-05-30 03:21:37 +0200

    La base de redireccionamiento

--- Shell/Shell_project.c
+++ Shell/Shell_project.c
@@ -23,6 +23,41 @@ To compile and run the program:
 //                            MAIN          
 // -----------------------------------------------------------------------
 
+static void parse_redirections(char **args, char **file_in, char **file_out) {
+	*file_in = NULL;
+	*file_out = NULL;
+
+	char **args_start = args;
+
+	while(*args) {
+		int is_in = !strcmp(*args, "<");
+		int is_out = !strcmp(*args, ">");
+		if (is_in || is_out) {
+			args++;
+			if (*args) {
+				if (is_in) *file_in = *args;
+				if (is_out) *file_out = *args;
+				char **aux = args + 1;
+				while(*aux) {
+					*(aux - 2) = *aux;
+					aux++;
+				}
+				*(aux - 2) = NULL;
+				args--;
+			} else {
+				/* Syntax error */
+				fprintf(stderr, "syntax error in redirection\n");
+				args_start[0] = NULL; // Do nothing
+			}
+		} else {
+			args++;
+		}
+	}
+	// Debug:
+	// *file_in && fprint(stderr, "[parse_redirections] file_in = '%s'\n", *file_in);
+	// *file_out && fprint(stderr, "[parse_redirections] file_out = '%s'\n", *file_out);
+}
+
 // La lista es una estructura global
 job *lista;
 
@@ -92,6 +127,13 @@ int main(void)
 		printf("COMMAND->");
 		fflush(stdout);
 		get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
+		
+		char *file_in = NULL;
+		char *file_out = NULL;
+		parse_redirections(args, &file_in, &file_out);
+
+		if (file_in) { printf("Hay redirección <: '%s'\n", file_in);}
+		if (file_out) { printf("Hay redirección >: '%s'\n", file_out);}
 		
 		if(args[0]==NULL) continue;   // if empty command
 
